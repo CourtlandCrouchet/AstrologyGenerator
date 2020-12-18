921d6616af7a7ce11208b559e6a2cf5e0bf04848
Courtland <crouchetcourtland@gmail.com>
2020-12-18 13:40:51 -0600

    Instantiated sentence data in sentences.js

--- js/sentences.js
+++ js/sentences.js
@@ -5,15 +5,80 @@
 * <asp> = sign aspect
 * <adj> = adjective
 */
+var fragments, structures, signs, aspects, types;
+
+function httpGet(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    callback(xmlHttp.responseText);
+}
+
+loadSentenceData()
+{
+    httpGet("json/sentence_fragments.json", function(response){
+        try {
+            fragments = JSON.parse(response);
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
+    });
+
+    httpGet("json/sentence_structures.json", function(response){
+        try {
+            structures = JSON.parse(response);
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
+    });
+
+    httpGet("json/sign_aspects.json", function(response){
+        try {
+            aspects = JSON.parse(response);
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
+    });
+    
+    httpGet("json/sign_types.json", function(response){
+        try {
+            types = JSON.parse(response);
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
+    });
+
+    httpGet("json/signs.json", function(response){
+        try {
+            signs = JSON.parse(response);
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
+    });
+}
+
 function matchSentence()
 {
 
 }
 function signSentence()
 {
-
+    
 }
 function horoscopeSentence()
 {
-    
+
 }
+
+loadSentenceData();
